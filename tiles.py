5916871e4fb0d5f8c9c45be8f89e1b008ce54c16
Natnael Teklu <natnaelteklu200@gmail.com>
2024-06-14 13:17:10 +0300

    Revert "touch"
    This reverts commit 02fcef0201e1504b787d3b63deba641ad24cc3a7.

--- tiles.py
+++ tiles.py
@@ -3,12 +3,10 @@ import pygame
 
 screen_width = 400
 class Tile(pygame.sprite.Sprite):
-    def __init__(self,x,y,size,color=(0,0,0),touched=False):
+    def __init__(self,x,y,size):
         super().__init__()
-        self.touched=touched
-        self.color=color
         self.image = pygame.Surface((size,size*2))
-        self.image.fill(self.color)
+        self.image.fill((0,0,0))
         self.rect = self.image.get_rect(topleft = (x,y))
     def update(self):
         self.rect.y += 10
